4e298807ab3fcd8b35cd7baff97a5c93a6a8e6c3
patrick_mugo <patrickkinyua2004@gmail.com>
2025-10-16 12:04:34 +0300

    new 5

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,7 @@
+-- File: task_5.sql
 USE alx_book_store;
 
-INSERT INTO  customers (customer_id,customer_name, email,address)
-  VALUES
-(1,
- 'Cole Baidoo',
- 'cbaidoo@sandtech.com',
-'123 Happiness Ave.'');
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
